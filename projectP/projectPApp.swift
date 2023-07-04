8e1c3d469be0f0248a6e988397fc92ef064a4e04
Wender <outrowender@gmail.com>
2023-07-04 17:15:13 -0300

    Initial Commit

---
+++ projectP/projectPApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectPApp.swift
+//  projectP
+//
+//  Created by Wender on 04/07/23.
+//
+
+import SwiftUI
+
+@main
+struct projectPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
