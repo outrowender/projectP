5a129d30cbcee32a089584824de0234c4c438057
Wender <outrowender@gmail.com>
2023-08-02 19:28:48 -0300

    added observable

---
+++ projectP/Extensions/Observable.swift
@@ -0,0 +1,8 @@
+//
+//  Observable.swift
+//  projectP
+//
+//  Created by Wender on 26/07/23.
+//
+
+import Foundation
---
+++ projectP/Views/PlayerView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayerView.swift
+//  projectP
+//
+//  Created by Wender on 25/07/23.
+//
+
+import SwiftUI
+
+struct PlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerView()
+    }
+}
